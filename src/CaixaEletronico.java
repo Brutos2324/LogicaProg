dec70583f5d8819cd88b4cad09f23526414d0063
Brutos2324 <fvlbrutos212@gmail.com>
2025-06-03 09:52:40 -0300

    Melhorando a Logica

---
+++ src/CaixaEletronico.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class CaixaEletronico {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Passo 1 = Receber o valor do saque
+        System.out.println("Digite o valor do saque");
+        int valor = scanner.nextInt();
+
+
+        //Passo 2 = Notas Disponiveis
+        int[] notas = {100, 50, 20, 10, 5, 2, 1,};
+        int[] quantidadeNotas = new int[notas.length];
+
+        //Passo 3 = Calcular a quantidade de cada nota
+        for (int i = 0; i < notas.length; i++){
+            if(valor >= notas[i]){
+                quantidadeNotas [i] = valor / notas[i];
+                valor %= notas[i];
+            }
+        }
+
+        //Passo 4 = Exibir o resultado
+        System.out.println("Notas Fornecidas: ");
+        for (int i = 0; i < notas.length; i++){
+            if (quantidadeNotas[i] > 0){
+                System.out.println(quantidadeNotas[i] + " notas(s) de " + notas[i]);
+            }
+        }
+
+        scanner.close();
+
+
+    }
+
+
+
+
+
+}
